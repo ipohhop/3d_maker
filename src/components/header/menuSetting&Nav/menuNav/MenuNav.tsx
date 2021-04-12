061d80c2eac77f3ba6c39111a762eba23f0b0b60
Artsem Latuohuski <artsem.latuohuski@innowise-group.com>
2021-04-12 17:48:17 +0300

    creat insert options

--- src/components/header/menuSetting&Nav/menuNav/MenuNav.tsx
+++ src/components/header/menuSetting&Nav/menuNav/MenuNav.tsx
@@ -2,10 +2,7 @@
 import React, {FunctionComponent, MutableRefObject} from 'react';
 
 //local
-import NavItem from "./NavItem";
 import "./menuNav.scss"
-import {useGlobalContext} from "../../../../App";
-import {ConstructorCanvas} from "../../../../threejs/constructorCanvas";
 import FileMenuItem from "./menuItems/FileMenuItem";
 import EditMenuItem from "./menuItems/EditMenuItem";
 import InsertMenuItem from "./menuItems/InsertMenuItem";
@@ -25,27 +22,6 @@ export type objectItem = {
 
 
 const MenuNav: FunctionComponent<Props> = (props) => {
-
-    const context = useGlobalContext()
-    let canvasObject = context.canvas as MutableRefObject<ConstructorCanvas>
-
-
-    type menuItemsType = { name: string, event: () => any, optionsItems: objectItem[] | any[] }
-
-    const menuItems: menuItemsType[] = [
-        {name: "File", event: () => null, optionsItems: []},
-        {name: "Edit", event: () => null, optionsItems: []},
-        {
-            name: "Insert", event: () => null, optionsItems: [
-                {name: "iPhone", event: canvasObject.current.addIphone},
-                {name: "iPad", event: canvasObject.current.addIpad},
-                {name: "Macbook", event: canvasObject.current.addMac}
-            ]
-        },
-        {name: "Background", event: () => null, optionsItems: []},
-        {name: "Advanced", event: () => null, optionsItems: []},
-    ]
-
 
     return (
         <nav className="header__nav">
